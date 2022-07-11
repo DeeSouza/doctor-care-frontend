0179961734e842205e842fe8f0fbcadd73b611cb
Diego Souza <dee-souza@hotmail.com>
2022-07-11 14:27:23 -0300

    trying change path assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "doctor-care-frontend",
   plugins: [react()],
   build: {
     outDir: "./docs",
